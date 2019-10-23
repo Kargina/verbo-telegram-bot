8c721f811ca215d8267b167efb1be529b725479c
Ksenia Kargina <kargina.ks@gmail.com>
2019-10-23 22:44:18 +0300

    words adding to db and auto translation

--- bot.py
+++ bot.py
@@ -1,41 +1,55 @@
 import telebot
 import os
+import sqlite3
+import logging
+from db import DB
+from googletrans import Translator
 
-api_key = "token"
+logging.basicConfig()
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
 
 
 def main():
+
+    # db = DB("jhjhj.hhjh")
+
+    api_key = os.getenv("TELEGRAM_TOKEN", "")
+
+    if not api_key:
+        log.info("Please set env var TELEGRAM_TOKEN")
+        exec(1)
+
     bot = telebot.TeleBot(api_key)
 
     @bot.message_handler(commands=['start', 'help'])
     def send_welcome(message):
-        bot.reply_to(message, "Howdy, how are you doing?")
+        bot.send_message(message.chat.id, "Howdy, how are you doing?")
 
     @bot.message_handler(commands=['dict'])
     def print_dict(message):
-        path_to_file = "data/" + str(message.chat.id)
-        dict_words = open(path_to_file)
-        # bot.reply_to(message, dict_words)
-        for line in dict_words:
-            bot.reply_to(message, line)
+        conn = sqlite3.connect("data/word_base.db")
+        cursor = conn.cursor()
+        user_id = message.chat.id
+        cursor.execute(f"select * from words where user_id = {user_id}")
+        user_dict = cursor.fetchall()
+        for line in user_dict:
+            bot.send_message(message.chat.id, line[1] + " - " + line[2])
 
     @bot.message_handler()
     def add_word(message):
-        bot.reply_to(message, "Adding word to dictionary. " + "\n" +
-                     "message.text: " + message.text + "\n"
-                     "message.id: " + str(message.message_id) + "\n"
-                     "message.chat.id: " + str(message.chat.id) + "\n")
-                     # "message.length: " + str(message.length) + "\n")
-        path_to_file = "data/" + str(message.chat.id)
-        if os.path.exists(path_to_file):
-            bot.reply_to(message, "User Exists!")
-            with open(path_to_file, 'a') as f:
-                print(message.text, file=f)
+        new_word = message.text.capitalize()
 
-        else:
-            bot.reply_to(message, "User NOT Exists!")
-            with open(path_to_file, 'w') as f:
-                print(message.text, file=f)
+        conn = sqlite3.connect("data/word_base.db")
+        cursor = conn.cursor()
+
+        translator = Translator()
+        translated_word = translator.translate(new_word, dest='ru').text.capitalize()
+
+        bot.send_message(message.chat.id, f"Adding word '{new_word}' as '{translated_word}' to dictionary. ")
+
+        cursor.execute(f"INSERT INTO words VALUES ({message.chat.id}, '{new_word}', '{translated_word}')")
+        conn.commit()
 
     bot.polling()
 
---
+++ db.py
@@ -0,0 +1,9 @@
+class DB():
+
+    def __init__(self, path):
+        pass
+
+
+    def create_db(self):
+        pass
+
