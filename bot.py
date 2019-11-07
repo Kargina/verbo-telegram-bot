d631d9e4fc3018cac39239c62aa49ef11fa60c68
Ksenia Kargina <kargina.ks@gmail.com>
2019-11-08 00:20:43 +0300

    some refactoring, send user dict as file

--- bot.py
+++ bot.py
@@ -1,67 +1,77 @@
 import telebot
-# from telebot import types
 import os
 import sqlite3
 import logging
 from googletrans import Translator
+import tempfile
+import json
 
 logging.basicConfig()
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 
 
-def main():
-    if not os.path.isdir("data"):
-        os.mkdir("data")
+def db_query(sql, commit=False):
     conn = sqlite3.connect("data/word_base.db")
     cursor = conn.cursor()
-    cursor.execute("""CREATE TABLE IF NOT EXISTS words
-                             (user_id integer, word text, translation text)
-                          """)
+    cursor.execute(sql)
+    if commit:
+        conn.commit()
     conn.close()
 
+
+def main():
+
+    if not os.path.isdir("data"):
+        os.mkdir("data")
+
+    db_query("""CREATE TABLE IF NOT EXISTS words
+                (user_id integer, word text, translation text)""")
+
     api_key = os.getenv("TELEGRAM_TOKEN", "")
+
     if not api_key:
         log.info("Please set env var TELEGRAM_TOKEN")
         exit(1)
 
     bot = telebot.TeleBot(api_key)
 
-    @bot.message_handler(commands=['start'])
-    def send_welcome(message):
-        """Test function"""
-        bot.send_message(message.chat.id, "Hi! How are you doing?")
-
     @bot.message_handler(commands=['dict'])
     def print_dict(message):
-        """Returns users dictionary
-           if command /dict provided"""
+        """
+        Returns users dictionary
+        if command /dict provided
+        """
         conn = sqlite3.connect("data/word_base.db")
         cursor = conn.cursor()
         user_id = message.chat.id
         cursor.execute(f"select * from words where user_id = {user_id}")
         user_dict = cursor.fetchall()
-        for line in user_dict:
-            bot.send_message(message.chat.id, line[1] + " - " + line[2])
-        conn.close()
+
+        bot.send_message(message.chat.id, json.dumps(user_dict, indent=4))
+
+        file, path = tempfile.mkstemp(suffix=".txt")
+        with open(path, "w") as f:
+            data = ""
+            for line in user_dict:
+                data = f"{data}\n{line[1]}:{line[2]}"
+            f.write(data)
+        with open(path, "r") as f:
+            bot.send_document(message.chat.id, f, caption="dict.txt")
 
     @bot.message_handler()
     def add_word(message):
-        """Add new word/phrase to dictionary
-           with it's translation"""
+        """
+        Add new word/phrase to dictionary
+        with it's translation
+        """
         new_word = message.text.capitalize()
-
-        conn = sqlite3.connect("data/word_base.db")
-        cursor = conn.cursor()
 
         translator = Translator()
         translated_word = translator.translate(new_word, dest='ru').text.capitalize()
+        bot.send_message(message.chat.id, f"Word {new_word!r} as {translated_word!r} added to dictionary. ")
 
-        bot.send_message(message.chat.id, f"Adding word '{new_word}' as '{translated_word}' to dictionary. ")
-
-        cursor.execute(f"INSERT INTO words VALUES ({message.chat.id}, '{new_word}', '{translated_word}')")
-        conn.commit()
-        conn.close()
+        db_query(f"INSERT INTO words VALUES ({message.chat.id}, '{new_word}', '{translated_word}')", commit=True)
 
     bot.polling()
 
